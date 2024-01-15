cdbaf5dc51876f750411601d7e58d96534e0afdf
mohamedhouidi <90357936+mohamedhouidi@users.noreply.github.com>
2024-01-15 22:13:23 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
   fineractPlatformTenantId: 'mobile',  // For connecting to server running elsewhere update the tenant identifier
-  //apiProvider: '/fineract-provider/api',
-  //apiVersion: '/v1',
-  //serverUrl: 'https://mobile.mifos.io/fineract-provider/api/v1',
   selfServiceRoleId: 2,
   baseApiUrl:'http://20.70.58.26:2000',
-  //apiProvider: '/fineract-provider/api',
-  //apiVersion: '/v1',
-  //serverUrl: 'https://mobile.mifos.io/fineract-provider/api/v1',
   serverUrl: 'http://20.70.58.26:2000',
 };
