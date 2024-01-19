06e029bba93f194b852bedef5b897d7baca61d1c
mohamedhouidi <90357936+mohamedhouidi@users.noreply.github.com>
2024-01-19 11:27:44 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 export const environment = {
   production: false,
   fineractPlatformTenantId: 'mobile', 
-  baseApiUrl:'http://20.37.4.143:2000',
+  baseApiUrl:'http://20.213.248.132:2000',
   
-  serverUrl: 'http://20.37.4.143:2000',
+  serverUrl: 'http://20.213.248.132:2000',
 
   selfServiceRoleId: 2
 };
