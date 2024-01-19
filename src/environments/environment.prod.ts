af0c66257e0f9ce994671ada54ad378add1e6fc4
mohamedhouidi <90357936+mohamedhouidi@users.noreply.github.com>
2024-01-19 11:27:13 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   fineractPlatformTenantId: 'mobile',  // For connecting to server running elsewhere update the tenant identifier
   selfServiceRoleId: 2,
-  baseApiUrl:'http://20.37.4.143:2000',
-  serverUrl: 'http://20.37.4.143:2000',
+  baseApiUrl:'http://20.213.248.132:2000',
+  serverUrl: 'http://20.213.248.132:2000',
 };
